c3f6cd27c2acc19c16121ea39b983f598bfbff7f
ajordon <airjordon2@gmail.com>
2016-01-31 16:09:42 -0500

    Switched from div to canvas. Coded place holders for game logic.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,6 +9,47 @@ require('./example');
 // load sass manifest
 require('../styles/index.scss');
 
+var canvas = document.getElementsById('gameboard');
+var ctx = canvas.getContent("2d");
+
+var global = {
+  score: [0,0]
+};
+
+ctx.fillStyple = "#fff";
+ctx.fill(10,10,10,100);
+
+var winAnnounce = function() {
+  var win = winListner();
+  switch (win) {
+    case "x":
+      alert("X Wins!");
+      global.score[0]++;
+      clearAll();
+      break;
+
+    case "o":
+      alert("O Wins!");
+      global.score[1]++;
+      clearAll();
+      break;
+
+    default:
+      break;
+  }
+
+  updateScore();
+  clearAll();
+};
+
+var updateScore = function() {};
+var userClick = function () {};
+var clearAll = function() {};
+
+vvar
+
 $(document).ready(() => {
+  canvas.addEventListener('click', userClick);
+  document.getElementsById('reset').addEventListener('click', clearAll);
   console.log('It works.');
 });
