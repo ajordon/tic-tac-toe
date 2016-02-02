4c58d9254153634292e487d7bf6fa8f1344e88ec
ajordon <airjordon2@gmail.com>
2016-02-02 18:23:06 -0500

    Completed board layout and placement of X and Os. Reverted back to divs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,32 +9,44 @@ require('./example');
 // load sass manifest
 require('../styles/index.scss');
 
-var canvas = document.getElementsById('gameboard');
-var ctx = canvas.getContent("2d");
-
-var global = {
-  board: [[1,2,3],
+let global = {
+  "board": [[1,2," "],
           [4,5,6],
           [7,8,9]],
-  score: [0,0],
-  turn: true; // true = X, false = O
+  "score": [0,0],
+  "turn": true, // true = X, false = O
+  "turnCount": 0
 };
 
-ctx.fillStyple = "#fff";
-ctx.fill(10,10,10,100);
+let updateScore = function() {
+  document.querySelector('.score').innerHTML = "	<small>X - </small>" + global.score[0] +
+  "/ <small> O -</small>"  + global.score[1];
+};
 
-var winAnnounce = function() {
-  var win = winListner();
+let clearAll = function() {
+  for (let i = 0; i < global.board[0].length; i++) {
+    for(let x = 0; x < global.board[1].length; i++) {
+      global.board[i][x] = " ";
+    }
+  }
+};
+
+let winAnnounce = function() {
+  // let win = winListner();
   switch (win) {
     case "x":
       alert("X Wins!");
       global.score[0]++;
+      updateScore();
+      global.turn = false;
       clearAll();
       break;
 
     case "o":
       alert("O Wins!");
       global.score[1]++;
+      updateScore();
+      global.turn = true;
       clearAll();
       break;
 
@@ -46,19 +58,24 @@ var winAnnounce = function() {
   clearAll();
 };
 
-var updateScore = function() {};
-var userClick = function () {};
-
-var clearAll = function() {
-  for (let i = 0; i < global.board[0].length; i++) {
-    for((let x = 0; x < global.board[1].length; i++) {
-      global.board[i][x] = " ";
-    }
-  }
-};
-
 $(document).ready(() => {
-  canvas.addEventListener('click', userClick);
-  document.getElementsById('reset').addEventListener('click', clearAll);
+  // $('.turn').innerHTML = global.turn //Show the turn as x or o at the start
+  $('#reset').on('click', clearAll);
+  $('#gameboard').on('click', function(event) {
+    event.preventDefault();
+    let gameboardBox = $(event.target);
+    if (global.turn === true) {
+      gameboardBox.text("X");
+      gameboardBox.css("background-color", "green");
+      global.turn = false;
+    } else {
+      gameboardBox.text("O");
+      gameboardBox.css("background-color", "blue");
+      global.turn = true;
+    }
+    global.turnCount++;
+    console.log(global.turnCount);
+  });
+  updateScore();
   console.log('It works.');
 });
